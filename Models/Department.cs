cf1b90be1bc3b780b0adc82e1d6df957317b988c
-SSS- <karpoff0502@gmail.com>
2025-06-02 13:06:15 +0300

    employees

--- Models/Department.cs
+++ Models/Department.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace EmployeeStruct.Models;
 
@@ -12,4 +13,6 @@ public partial class Department
     public string? Description { get; set; }
 
     public virtual ICollection<DepartmentSubdivision> DepartmentSubdivisions { get; set; } = new List<DepartmentSubdivision>();
+    
+    public IEnumerable<object> Items => DepartmentSubdivisions.Cast<object>();
 }
